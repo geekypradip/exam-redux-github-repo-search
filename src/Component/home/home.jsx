a2088586b8f22bbd7a6297361d9703a7cceec99a
pradip mandal <join2me.pm@gmail.com>
2022-01-05 22:54:46 +0530

    craeated home page

--- src/Component/home/home.jsx
+++ src/Component/home/home.jsx
@@ -1,14 +1,30 @@
-function Home() {
-    return ( 
-        <div>
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import styles from '../style/style.module.css'
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
-            Home page
+export default function Home() {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+        <div className={styles.inputHolder}>
+            <TextField id="outlined-basic" label="search any git repo" variant="outlined" 
+      className={styles.inputFiled}/>
+      <Button variant="contained" color="primary" className={styles.inputButton}>
+              Search
+     </Button>
         </div>
-     );
+      
+    </form>
+  );
 }
-
-export default Home;
-<div>
-
-    Home page
-</div>
